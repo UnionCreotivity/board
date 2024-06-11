727155cc5f8cba89dafaaf5502e9d39099766cf4
Louis <p19970408@gmail.com>
2024-06-11 16:01:31 +0800

    Updates

--- components/home/FifthCut.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <article class="home-cut home-cut-5">
-    <h1 style="color: white; text-align: center">我是slide-4</h1>
-  </article>
-</template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
